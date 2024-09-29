59f77d2a1ca0eba9f0732ead3e5ec8b76a501d6e
Varon98 <varonmclaren@gmail.com>
2024-09-30 02:58:29 +0500

    create filter with polyline

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+  Polyline,
+} from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { useQuery } from "@tanstack/react-query";
 import { fetchUsers } from "./service/query.service";
+import FilterResult from "./util/filter";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
@@ -16,11 +24,12 @@ const icon = new L.Icon({
 function App() {
   const [userLocation, setUserLocation] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [openFilter, setOpenFilter] = useState(false);
+  const [positions, setPositions] = useState([]);
   const { data = [], isLoading } = useQuery({
     queryKey: ["users"],
     queryFn: () => fetchUsers("get/all"),
   });
-  console.log(userLocation);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -33,7 +42,7 @@ function App() {
           setLoading(false);
         },
         (error) => {
-          console.error("Ошибка получения геолокации: ", error);
+          console.error("Geolocation error: ", error);
           setLoading(false);
         }
       );
@@ -43,62 +52,125 @@ function App() {
   }, []);
 
   if (loading) {
-    return <div>Загрузка карты...</div>;
+    return <div>Loading map...</div>;
   }
 
-  return (
-    <MapContainer
-      center={[33.547119140625, -7.675495147705078]}
-      zoom={13}
-      style={{ height: "100vh", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <MarkerClusterGroup chunkedLoading>
-        {!isLoading && (
-          <>
-            {data?.innerData?.map((user) => {
-              const position = JSON.parse(user?.position);
+  const isPointInPolygon = (point, polygon) => {
+    const latlng = L.latLng(point[0], point[1]);
+    const bounds = L.latLngBounds(polygon.map((p) => L.latLng(p?.lat, p?.lng)));
+    return bounds.contains(latlng);
+  };
 
-              return (
-                <Marker
-                  key={user?.id}
-                  position={position[0] != null ? position : null}
-                  icon={icon}
-                >
-                  <Popup>
-                    <p>{user?.property_type}</p>
-                    <small>district: {user?.district}</small>
-                    <br />
-                    <small>elevator: {user?.elevator ? "yes" : "no"}</small>
-                    <br />
-                    <small>room number: {user?.rooms_number}</small>
-                    <br />
-                    <small>{user?.publish_date}</small>
-                    <br />
-                    <small>{user?.title}</small>
-                    <br />
-                    <details>
-                      <summary>description</summary>
-                      <small>{user?.description}</small>
-                    </details>
-                    <br />
-                    <p>
-                      see {user?.property_type} —{" "}
-                      <a href={user?.url} target="blank">
-                        here
-                      </a>
-                    </p>
-                  </Popup>
-                </Marker>
-              );
-            })}
+  const filterPointsInPolygon = (dataPoints) => {
+    const polygon = positions;
+    if (positions?.length < 4) {
+      return;
+    }
+    return dataPoints.filter((point) => {
+      if (!point?.position) {
+        return false;
+      }
+      try {
+        const position = JSON.parse(point.position);
+        if (!position || position.length < 2) {
+          console.error("Invalid position data:", position);
+          return false;
+        }
+        return isPointInPolygon(position, polygon);
+      } catch (error) {
+        console.error("Error parsing position:", error);
+        return false;
+      }
+    });
+  };
+
+  const MapClickHandler = () => {
+    useMapEvents({
+      click(e) {
+        const newPosition = e.latlng;
+        setPositions((prev) => [...prev, newPosition]);
+        console.log("Clicked coordinates:", e.latlng); // Koordinatları konsola yazdır
+      },
+    });
+    return null;
+  };
+
+  const closeFilter = () => {
+    setOpenFilter(!openFilter);
+    setPositions([]);
+  };
+
+  return (
+    <>
+      <MapContainer
+        center={[33.547119140625, -7.675495147705078]}
+        zoom={13}
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <FilterResult
+          data={openFilter ? filterPointsInPolygon(data?.innerData || []) : []}
+          open={openFilter}
+          setOpen={() => closeFilter()}
+        />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {openFilter && (
+          <>
+            {" "}
+            <MapClickHandler />
+            {positions.length > 0 && (
+              <Polyline positions={positions} color="blue" />
+            )}
+            {positions.map((position, index) => (
+              <Marker key={index} position={position} />
+            ))}
           </>
         )}
-      </MarkerClusterGroup>
-    </MapContainer>
+        <MarkerClusterGroup chunkedLoading>
+          {!isLoading && (
+            <>
+              {data?.innerData?.map((user) => {
+                const position = JSON.parse(user?.position);
+                return (
+                  <Marker
+                    key={user?.id}
+                    position={position[0] != null ? position : null}
+                    icon={icon}
+                  >
+                    <Popup>
+                      <p>{user?.property_type}</p>
+                      <small>district: {user?.district}</small>
+                      <br />
+                      <small>elevator: {user?.elevator ? "yes" : "no"}</small>
+                      <br />
+                      <small>room number: {user?.rooms_number}</small>
+                      <br />
+                      <small>{user?.publish_date}</small>
+                      <br />
+                      <small>{user?.title}</small>
+                      <br />
+                      <details>
+                        <summary>description</summary>
+                        <small>{user?.description}</small>
+                      </details>
+                      <br />
+                      <p>
+                        see {user?.property_type} —{" "}
+                        <a href={user?.url} target="blank">
+                          here
+                        </a>
+                      </p>
+                    </Popup>
+                  </Marker>
+                );
+              })}
+            </>
+          )}
+        </MarkerClusterGroup>
+      </MapContainer>
+    </>
   );
 }
 
